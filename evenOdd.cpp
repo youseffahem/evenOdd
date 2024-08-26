f37038dc8bbb63d1826f67aea284129d53ffa014
yousef_fahem <yousef.fahem11@gmail.com>
2024-08-26 12:11:10 +0300

    Create evenOdd.cpp

---
+++ evenOdd.cpp
@@ -0,0 +1,11 @@
+ //that takes a number from user and checks if this number is even or odd.
+
+int num;
+ cout << "enter the number\n";
+ cin >> num;
+ if (num % 2 == 0) {
+ cout << "the number is even\n";
+ }
+ else if(num%2!=0){
+ cout << "the number is odd\n";
+ }
